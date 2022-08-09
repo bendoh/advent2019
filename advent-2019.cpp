10b8e9e6f75ff85e869b9678b1ab174b0a6331ee
Benjamin Doherty <bendohmv@gmail.com>
2022-08-09 11:54:19 -0400

    Day 4!

--- advent-2019.cpp
+++ advent-2019.cpp
@@ -455,28 +455,41 @@ public:
 
   string day4() {
     vector<string> range = regex_split(input_lines[0], "-");
-    vector<string> matches;
+    vector<string> part1_matches, part2_matches;
     uint32_t lb = atoi(range[0].c_str()), ub = atoi(range[1].c_str());
 
     for(uint32_t i = lb; i <= ub; i++) {
       string s = to_string(i);
+      char doubled_digit = 0;
       bool has_double = false;
       bool no_decrease = true;
 
       for (uint32_t v = 0; v < s.length() - 1; v++) {
-        if(s[v] == s[v+1]) has_double = true;
+        if(s[v] == s[v+1]) {
+          has_double = true;
+          if(
+              (v == 0 || s[v-1] != s[v]) &&
+              (v == s.length() - 2 || s[v+2] != s[v])
+          )
+            doubled_digit = s[v];
+        }
         if(s[v] > s[v+1]) no_decrease = false;
       }
 
       if (has_double && no_decrease) {
-        cout << "Match found: " << s << "\n";
+        part1_matches.push_back(s);
+      }
 
-        matches.push_back(s);
+      if (no_decrease && doubled_digit) {
+        part2_matches.push_back(s);
       }
     }
 
     day_complete = true;
-    return to_string(matches.size());
+    return (
+      "Part1: " + to_string(part1_matches.size()) + "\n" +
+      "Part2: " + to_string(part2_matches.size())
+    );
 
   }
 
